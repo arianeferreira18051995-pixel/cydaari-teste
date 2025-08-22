9f5b7f7aa89685b7187d3824a66d57655fbbc8f2
Ariane Ferreira Cortezani <arianeferreira18051995@gmail.com>
2025-08-22 14:54:11 -0300

    Novos Testes da Ari3

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "y3ao79",
+  projectId: "rt6hfk",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
